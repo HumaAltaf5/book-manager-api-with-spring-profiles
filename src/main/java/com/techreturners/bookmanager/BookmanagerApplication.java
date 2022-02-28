de2ac1a4232edac5af873cdf9ea6153359d0172c
HumaAltaf5 <huma.altaf@gmail.com>
2022-02-28 12:28:32 +0000

    Adding code to implement documentation for the API using swagger

--- src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
+++ src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
@@ -2,6 +2,14 @@ package com.techreturners.bookmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class BookmanagerApplication {
@@ -10,4 +18,22 @@ public class BookmanagerApplication {
 		SpringApplication.run(BookmanagerApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.withClassAnnotation
+						(RestController.class))
+				.build()
+				.apiInfo(apiInformation());
+	}
+
+	private ApiInfo apiInformation() {
+		return new ApiInfoBuilder()
+				.title(" The Minimalist Book Manager API")
+				.description("For all the Book lovers out there, this API helps to manage all books at your finger tips!")
+				.version("v1")
+				.build();
+	}
 }
